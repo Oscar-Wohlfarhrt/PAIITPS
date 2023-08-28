8f971b74ffdfbb8e439ae6f375671a825121faf3
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2023-08-28 15:15:13 -0300

    First commit

---
+++ src/main/java/com/enderfire/paiitps/PAIITPS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.enderfire.paiitps;
+
+/**
+ *
+ * @author Oscar2
+ */
+public class PAIITPS {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new MainForm().setVisible(true));
+    }
+}
